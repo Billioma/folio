f216948d7f4fba2bdcec8469ba7cc18f41d61b59
Billioma <ibeomari@gmail.com>
2022-12-18 20:03:42 +0100

    .

--- src/components/container/About/About.jsx
+++ src/components/container/About/About.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 import "./About.scss";
-import { motion } from 'framer-motion';
-import { bios } from '../../../Data';
-import main from "../../../assets/main.jpeg"
+import { motion } from "framer-motion";
+import { bios } from "../../../Data";
+import main from "../../../assets/main.jpeg";
 
 const About = () => {
-
   return (
-    <div className="container " id='about'>
+    <div className="container" id="about">
       <motion.div
         initial={{ opacity: 0 }}
         whileInView={{ y: [-50, 0], opacity: 1 }}
-        className="title">
+        className="title"
+      >
         <span>Who Am I?</span>
         <h1>About Me</h1>
       </motion.div>
@@ -21,39 +21,51 @@ const About = () => {
           initial={{ x: 0, opacity: 0 }}
           whileInView={{ x: [-250, 0], opacity: 1 }}
           transition={{ duration: 1 }}
-          className="about_left">
-          <motion.img src={main}
+          className="about_left"
+        >
+          <motion.img
+            src={main}
             whileHover={{ y: -48, x: -55 }}
             transition={{ duration: 0.3 }}
           />
         </motion.div>
-        <motion.div className="about_right"
+        <motion.div
+          className="about_right"
           initial={{ x: 0, opacity: 0 }}
           whileInView={{ x: [250, 0], opacity: 1 }}
           transition={{ duration: 1 }}
         >
-
-          <p>Front-End Developer with proven experience to collaborate effectively with Back-End Developers and Web Designers in helping companies/brands create and maintain a better code base for reusability. I look forward to working collaboratively.</p>
-          {bios.map(bio => {
+          <p>
+            Front-End Developer with proven experience to collaborate
+            effectively with Back-End Developers and Web Designers in helping
+            companies/brands create and maintain a better code base for
+            reusability. I look forward to working collaboratively.
+          </p>
+          {bios.map((bio) => {
             return (
               <div className="bio" key={bio.id}>
-                <span className='bioKey'>{bio.icon}{bio.key}</span>
-                <a href={bio.link} className='bioValue'><span className='bioValue'>{bio.value}</span></a>
+                <span className="bioKey">
+                  {bio.icon}
+                  {bio.key}
+                </span>
+                <a href={bio.link} className="bioValue">
+                  <span className="bioValue">{bio.value}</span>
+                </a>
               </div>
-            )
+            );
           })}
-          <motion.a href='#' download=""
+          <motion.a
+            href="#"
+            download=""
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.3 }}
           >
             Download Resume
           </motion.a>
         </motion.div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
